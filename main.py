f7188af76215d7c80f7085ae854cff37f899883d
nilirias <33463564+nilirias@users.noreply.github.com>
2023-06-12 03:59:17 +0000

    function quads created

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class MikuParser(Parser):
     debugfile = 'parser.out'
     tokens = MikuLexer.tokens
     start = 'program'
+    id = ''
 
     and_pattern = re.compile("^([A-Z][0-9]+)+$")
 
@@ -16,7 +17,8 @@ class MikuParser(Parser):
     quadcount = 1
 
     myQuad = Quadruple(0, 0, 0, 0)
-    conQuad = Quadruple(0, 0, 0, 0)  
+    conQuad = Quadruple(0, 0, 0, 0) 
+    paramaQuad = Quadruple(0,0,0,0)
 
     operandos = []
     operadores = []
@@ -24,6 +26,7 @@ class MikuParser(Parser):
     jump = []
 
     temporal = 0
+    paramCont = 0
 
     # Grammar rules and actions
     @_('DRAWING ID \n declaration')
@@ -47,11 +50,15 @@ class MikuParser(Parser):
     def var_type(self, p):
         return p[0]
 
-    @_('func_type FUNC ID OPEN_PTH parameter CLOSE_PTH \n stmnt END \n',
+    @_('FUNC func_type ID fd1 OPEN_PTH parameter CLOSE_PTH \n stmnt END \n',
        'empty')
     def func_declaration(self, p):
         #print(f'func_declaration {p[0]}')
         return p[0]
+
+    @_('')
+    def fd1(self, p):
+      self.id = p[-1]
 
     @_('VOID', 'NUMBER', 'WORD', 'BOOL')
     def func_type(self, p):
@@ -81,8 +88,6 @@ class MikuParser(Parser):
 
     @_('ID e5 assign e6 expression q3 \n')
     def var_assignation(self, p):
-        #self.operandos.append(p[-1])
-        #print(f'var assign {p[0]}')
         return p[0]
 
     @_('')
@@ -95,19 +100,12 @@ class MikuParser(Parser):
         #print(f'e6 {p[-1]}')
         self.operadores.append(p[-1])
 
-    # @_('ID')
-    # def id(self, p):
-    #     return p[0]
-
     @_('ASSIGN')
     def assign(self, p):
         return p[0]
 
     @_('variable', 'CTE_NUM', 'CTE_STR', 'cte_bool', 'func_call')
     def var_cte(self, p):
-        #print(f'var_cte {p[-1]}')
-        #self.operandos.append(p[-1])
-        ##print(p[0])
         return p[0]
 
     @_('TRUE', 'FALSE')
@@ -116,15 +114,12 @@ class MikuParser(Parser):
 
     @_('exp rel_op exp q4', 'exp q4')
     def expression(self, p):
-        #print("expression AAAAAAAAAAAAAAAA")
         return p[-1]
 
     @_('')
     def q4(self, p):
-        #print(f'q4 {self.operadores} {self.operandos}')
         while len(self.operadores) > 0:
             op = self.operadores.pop()
-            #print(f'q4 op: {op}')
             if (op == '>' or op == '<' or op == '<=' or op == '>='
                     or op == '==' or op == '<>'):
                 lo = self.operandos.pop()
@@ -141,10 +136,8 @@ class MikuParser(Parser):
     #Punto neuralgico para expression
     @_('')
     def e4(self, p):
-        #print(f'e4 before {self.operadores}')
         self.operadores.append(p[-1])
         self.operadores.append('tururururu')
-        #print(f'e4 {self.operadores}')
 
     @_('termino term_op e3 termino q1', 'termino q1')
     def exp(self, p):
@@ -155,28 +148,21 @@ class MikuParser(Parser):
     @_('')
     def e3(self, p):
         self.operadores.append(p[-1])
-        #print(f'e3 {self.operadores}')
 
     #quads suma resta
     @_('')
     def q2(self, p):
-        #print(f'q2 {self.operadores} {self.operandos}')
         while len(self.operadores) > 0:
             op = self.operadores.pop()
-            #print(f'q2 op:{op}')
             if (op == '*' or op == '/'):
-                #print("q2 yeiii")
                 lo = self.operandos.pop()
                 ro = self.operandos.pop()
                 self.temporal = self.temporal + 1
                 self.operandos.append(self.temporal)
-                #print(f'q2 {lo, ro, op, self.temporal}')
-                #print(f'q2 append temp {self.operandos}')
                 myQuad = Quadruple(lo, ro, op, self.temporal)
                 self.quadruples.append(myQuad)
                 self.quadcount = self.quadcount + 1
             else:
-                #print("q2 unu")
                 self.operadores.append(op)
                 return
 
@@ -186,26 +172,20 @@ class MikuParser(Parser):
 
     @_('factor fact_op e2 expression q2', 'factor q2')
     def termino(self, p):
-        #print('termino no cerooooooo')
-        if (len(self.operandos) == 0):
-            print('termino')
+        # if (len(self.operandos) == 0):
+        #     print('termino')
         return p[-1]
 
 #quads mult div
-
     @_('')
     def q1(self, p):
-        #print(f'q1 {self.operadores} {self.operandos}')
         if (len(self.operadores) > 0):
             op = self.operadores.pop()
             if (op == '+' or op == '-'):
                 lo = self.operandos.pop()
                 ro = self.operandos.pop()
-                #print(f'q1 temporal {self.temporal} {type(self.temporal)}')
                 self.temporal = self.temporal + 1
                 self.operandos.append(self.temporal)
-                #print(f'q1 {lo, ro, op, self.temporal}')
-                #print(f'q1 append temp {self.operandos}')
                 myQuad = Quadruple(lo, ro, op, self.temporal)
                 self.quadruples.append(myQuad)
                 self.quadcount = self.quadcount + 1
@@ -214,16 +194,12 @@ class MikuParser(Parser):
 
     @_('')
     def q3(self, p):
-        #print(f'q3 {self.operadores} {self.operandos}')
         if (len(self.operadores) > 0):
             op = self.operadores.pop()
             if (op == '='):
                 lo = self.operandos.pop()
                 ro = None
                 temp = self.operandos.pop()
-                #print(f' 174 {len(self.operandos)}')
-                #print(f'q3 {lo, ro, op, temp}')
-                #print(f'q3 append temp {self.operandos}')
                 myQuad = Quadruple(lo, ro, op, temp)
                 self.quadruples.append(myQuad)
                 self.quadcount = self.quadcount + 1
@@ -233,9 +209,7 @@ class MikuParser(Parser):
     #Punto neuralgico para terminos
     @_('')
     def e2(self, p):
-        #print(p[-1])
         self.operadores.append(p[-1])
-        #print(f'e2 {self.operadores}')
 
     @_('MULT', 'DIV')
     def fact_op(self, p):
@@ -243,7 +217,6 @@ class MikuParser(Parser):
 
     @_('open_pth expression close_pth', 'var_cte e1')
     def factor(self, p):
-        #print(f'factor {p[-1]}')
         return p[-1]
 
     #Punto neuralgico para todos los operandos
@@ -269,25 +242,42 @@ class MikuParser(Parser):
 
     @_('AND', 'OR')
     def log_op(self, p):
-        #print(f'log_op {p[0]}')
-        #self.operadores.append(p[-1])
-        ##print(self.operadores)
         return p[0]
 
     @_('LESS_THAN', 'MORE_THAN', 'DIFFERENT_TO', 'LESS_OR_EQ_THAN',
        'MORE_OR_EQ_THAN', 'EQUAL_TO')
     def rel_op(self, p):
-        ##print(f'rel_op {p[0]}')
         self.operadores.append(p[-1])
-        ##print(self.operadores)
 
-    @_('ID OPEN_PTH func_call_param CLOSE_PTH \n')
+    @_('ID OPEN_PTH func1 func_call_param CLOSE_PTH func3 \n')
     def func_call(self, p):
+        self.id = p.ID
         return p[0]
 
-    @_('expression multiple_fc_param')
+    @_('')
+    def func1(self, p):
+      self.myQuad = Quadruple(self.id, None, 'ERA', None)
+      self.quadruples.append(self.myQuad)
+      self.quadcount = self.quadcount + 1
+      #print(self.myQuad)
+
+    @_('expression func2 multiple_fc_param')
     def func_call_param(self, p):
         return p[0]
+
+    @_('')
+    def func2(self, p):
+      self.paramCont = self.paramCont + 1
+      self.paramQuad = Quadruple(self.operandos.pop(), None, 'Parameter', self.paramCont)
+      self.quadruples.append(self.paramQuad)
+      self.quadcount = self.quadcount + 1
+      #print(self.paramQuad)
+
+    @_('')
+    def func3(self, p):
+      self.myQuad = Quadruple(self.id, None, 'GOSUB', None)
+      self.quadruples.append(self.myQuad)
+      self.quadcount = self.quadcount + 1
 
     @_('COMMA func_call_param', 'empty')
     def multiple_fc_param(self, p):
@@ -327,15 +317,13 @@ class MikuParser(Parser):
       self.returnquad.append(len(self.quadruples))
       self.quadruples.append(self.conQuad)
       
-
     @_('')
     def if2(self, p):
       self.jump.append(self.quadcount-1)
+      self.operandos.append(self.temporal)
       self.conQuad = Quadruple(self.operandos.pop(), None, 'gotof', self.jump)
       idx = self.returnquad.pop()
       self.quadruples[idx] = self.conQuad
-      # print(f'if2 {self.conQuad}')
-      # print(self.jump)
 
     @_('\n ELSE \n stmnt END \n', '\n END \n')
     def else_stmnt(self, p):
@@ -353,27 +341,20 @@ class MikuParser(Parser):
     @_('')
     def e7(self, p):
         self.operadores.append(p[-1])
-        #print(f' e7 {self.operadores}')
 
     @_('')
     def q5(self, p):
-        #print(f'q5 {self.operadores} {self.operandos}')
         while len(self.operadores) > 0:
             op = self.operadores.pop()
-            #print(f'q5 op:{op}')
             if (op.lower() == 'and' or op.lower() == 'or'):
-                #print("q5 yeiii")
                 lo = self.operandos.pop()
                 ro = self.operandos.pop()
                 self.temporal = self.temporal + 1
                 self.operandos.append(self.temporal)
-                #print(f'q5 {lo, ro, op, self.temporal}')
-                #print(f'q5 append temp {self.operandos}')
                 myQuad = Quadruple(lo, ro, op, self.temporal)
                 self.quadruples.append(myQuad)
                 self.quadcount = self.quadcount + 1
             else:
-                #print("q5 unu")
                 self.operadores.append(op)
                 return
 
@@ -407,7 +388,7 @@ class MikuParser(Parser):
 if __name__ == '__main__':
     lexer = MikuLexer()
     parser = MikuParser()
-    filename = 'test_rel.txt'
+    filename = 'test_modulos.txt'
 
     with open(filename) as fp:
         try:
